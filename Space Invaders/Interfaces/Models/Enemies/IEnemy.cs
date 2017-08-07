17af26470cd6aafbe0e2d60edc9965b2311c3b1b
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-08-07 06:01:31 +0300

    Added prop in IEnemy bool IsAlive. Added new interface IInvader and new class enemy.

--- Space Invaders/Interfaces/Models/Enemies/IEnemy.cs
+++ Space Invaders/Interfaces/Models/Enemies/IEnemy.cs
@@ -4,6 +4,6 @@
 
     public interface IEnemy : IEntity
     {
-
+       bool IsAlive { get;}
     }
 }
---
+++ Space Invaders/Models/Enimies/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Space_Invaders.Interfaces.Models.Enemies;
+
+namespace Space_Invaders.Models.Enimies
+{
+    public abstract class Enemy : IEnemy
+    {
+        public Texture2D Texture { get; }
+        public Rectangle Rectangle { get; }
+        public void Load(ContentManager content, GraphicsDevice GraphicsDevice, string path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime, KeyboardState keyboardState)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsAlive { get; }
+    }
+}
