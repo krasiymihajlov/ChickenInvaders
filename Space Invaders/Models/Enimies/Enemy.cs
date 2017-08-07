9a1dfec230ad7eea40c4bcb0807168b1e8e74e56
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-08-07 06:31:58 +0300

    Edited abstract class Enemy to inharit Entity and IEnemy.

--- Space Invaders/Models/Enimies/Enemy.cs
+++ Space Invaders/Models/Enimies/Enemy.cs
@@ -3,26 +3,18 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Space_Invaders.Interfaces.Models.Enemies;
+using Space_Invaders.Models.Entities;
 
 namespace Space_Invaders.Models.Enimies
 {
-    public abstract class Enemy : IEnemy
+    public abstract class Enemy : Entity, IEnemy
     {
-        public Texture2D Texture { get; }
-        public Rectangle Rectangle { get; }
-        public void Load(ContentManager content, GraphicsDevice GraphicsDevice, string path)
+        public Enemy(int x, int y) : base(x, y)
         {
-            throw new System.NotImplementedException();
         }
 
-        public void Update(GameTime gameTime, KeyboardState keyboardState)
+        public Enemy(int x, int y, int width, int height) : base(x, y, width, height)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            throw new System.NotImplementedException();
         }
 
         public bool IsAlive { get; }
