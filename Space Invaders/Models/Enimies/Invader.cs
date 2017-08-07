a25d4946745b4f582be20a8badd59be5dfe84c12
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-08-07 06:38:20 +0300

    Added class Invader

---
+++ Space Invaders/Models/Enimies/Invader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Space_Invaders.Interfaces.Models.Enemies;
+
+namespace Space_Invaders.Models.Enimies
+{
+    public class Invader : Enemy, IInvader
+    {
+        public Invader(int x, int y) : base(x, y)
+        {
+        }
+
+        public Invader(int x, int y, int width, int height) : base(x, y, width, height)
+        {
+        }
+
+        public override void Update(GameTime gameTime, KeyboardState keyboardState)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
