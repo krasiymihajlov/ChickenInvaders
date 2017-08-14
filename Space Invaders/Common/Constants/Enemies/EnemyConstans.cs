e803c50fbf49ab0e31e4661f1c268d3f6e18a5c1
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-08-14 13:17:00 +0300

    Edited EnemyConstants and EntityConstants and InvaderArmy so there is space between the invaders now.

--- Space Invaders/Common/Constants/Enemies/EnemyConstans.cs
+++ Space Invaders/Common/Constants/Enemies/EnemyConstans.cs
@@ -4,5 +4,9 @@
     {
         public const int Rows = 3;
         public const int Cols = 4;
+
+        public const int Enemy2Width = 72;
+        public const int Enemy2Height = 56;
+        public const int SpaceBetweenEnemies = 10;
     }
 }
--- Space Invaders/Common/Constants/Entities/EntityConstants.cs
+++ Space Invaders/Common/Constants/Entities/EntityConstants.cs
@@ -7,7 +7,5 @@
         public const int ShipWidth = 100;
         public const int ShipHeight = 60;
         public const int ShipSpeed = 3;
-        public const int Enemy2Width = 72;
-        public const int Enemy2Height = 56;
     }
 }
