82d356a6045d5b5d6c99bb5c7df80b846aad9749
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-08-15 01:15:25 +0300

    Edited GraphicsConstants.cs : ViewportWidth = 1024.

--- Space Invaders/Common/Constants/Graphics/GraphicsConstants.cs
+++ Space Invaders/Common/Constants/Graphics/GraphicsConstants.cs
@@ -5,7 +5,7 @@
         public const int PrefferBufferWidth = 1100;
         public const int PrefferBufferHeight = 700;
 
-        public const int ViewportWidth = 800;
+        public const int ViewportWidth = 1024;
         public const int ViewportHeight = 450;
 
         public const bool IsMouseVisible = true;
